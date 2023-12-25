47cafd718be553a49319a4c7d87b65383eda556f
Jonathan Henrique <jonathanbispo085@gmail.com>
2023-12-25 13:28:45 -0300

    Added JavaScript script to handle expanding/collapsing answers, and completed the mobile version.

---
+++ script.js
@@ -0,0 +1,13 @@
+const expandButtons = document.querySelectorAll("#expand")
+const answers = document.querySelectorAll(".answer")
+expandButtons.forEach((button, index) => {
+    button.addEventListener('click', ()=>{
+        if(button.src.includes('icon-minus.svg')){
+            button.src = 'assets/images/icon-plus.svg'
+            answers[index].style.display = 'none'
+        } else {
+            button.src = 'assets/images/icon-minus.svg'
+            answers[index].style.display = 'flex'
+        }
+    })
+})
